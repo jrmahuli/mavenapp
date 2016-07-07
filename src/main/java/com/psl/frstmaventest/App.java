bbd059d6222c125981880c5ae016c54c6d5a28ed
jrmahuli <jrmahuli@gmail.com>
2016-07-07 19:16:49 +0530

    Add files via upload

---
+++ src/main/java/com/psl/frstmaventest/App.java
@@ -0,0 +1,14 @@
+package com.psl.frstmaventest;
+import org.slf4j.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       Logger logger = LoggerFactory.getLogger(App.class);
+	   logger.info("Hello world");
+    }
+}
